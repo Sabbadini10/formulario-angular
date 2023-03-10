d06dd7a14ebb5c4ce8f6d824c29eb1cf02289f51
Sabbadini10 <matiassabbadini24@gmail.com>
2023-03-10 16:39:19 -0300

    my first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FormularioReactivoModule } from './formulario-reactivo/formulario-reactivo.module';
+import { FormularioTemplateModule } from './formulario-template/formulario-template.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormularioTemplateModule,
+    FormularioReactivoModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/formulario-reactivo/formulario-reactivo.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormularioReactivoComponent } from './formulario-reactivo/formulario-reactivo.component';
+
+
+
+@NgModule({
+  declarations: [
+    FormularioReactivoComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    FormularioReactivoComponent
+  ]
+})
+export class FormularioReactivoModule { }
