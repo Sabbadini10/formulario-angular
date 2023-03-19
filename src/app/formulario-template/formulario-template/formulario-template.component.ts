cb056e128ae67ef1d2d153515f7249f1f70a9408
Sabbadini10 <matiassabbadini24@gmail.com>
2023-03-19 19:07:30 -0300

    bug de formulario template arreglado

--- src/app/formulario-template/formulario-template/formulario-template.component.ts
+++ src/app/formulario-template/formulario-template/formulario-template.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-formulario-template',
@@ -7,4 +8,50 @@ import { Component } from '@angular/core';
 })
 export class FormularioTemplateComponent {
 
+  @ViewChild('formularioTemplate')
+
+  formularioTemplate! : NgForm;
+
+  initialState = {
+proyecto: '',
+horas: '0',
+tecnologia: ''
+  }
+  
+  tecnologias: Array<string> = [];
+  
+  proyectos: any[] = [];
+
+  agregarProyecto(){
+    if( this.formularioTemplate.invalid){
+      return;
+    }
+
+    this.proyectos.push({
+      proyecto: this.formularioTemplate.controls['proyecto'].value,
+      horas: this.formularioTemplate.controls['horas'].value,
+      tecnologia: this.tecnologias,
+    })
+
+    this.formularioTemplate.resetForm();
+    this.tecnologias = [];
+
+    console.log(this.proyectos)
+  }
+
+  agregarTec(){
+  
+    if( this.formularioTemplate.controls['tecnologia'].invalid){
+      return;
+    }
+     
+      this.tecnologias.push(this.formularioTemplate.controls['tecnologia'].value);
+
+    this.formularioTemplate.resetForm({ 
+      ...this.formularioTemplate.value,
+     tecnologia: ''
+    
+    }
+    );
+  }
 }
